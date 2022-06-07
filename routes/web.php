b575d79757283278d6f1a1c5f88e700285f9c94a
Magnus Macbook Wahyu <yasminrifdaniar@DJayantos-MacBook-Air.local>
2022-06-07 10:35:09 +0700

    first update

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::view('/about', 'about')->name('about');
+});
+
+
+
+require __DIR__.'/auth.php';
